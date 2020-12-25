5188d4dba2c5ee0f7af0716cba6898b4dad8e2fb
zhoukongcheng <zhoukongcheng@zhoukongchengdeMacBook-Air.local>
2020-12-25 16:28:35 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    // "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    // "plugin:vue/vue-recommended", // Use this if you are using Vue.js 2.x.
+  ],
+  rules: {
+    "vue/no-multiple-template-root": "off",
+  }
+}
